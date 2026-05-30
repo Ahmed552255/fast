95371486e1da568fd37bd0857ce406c73e05c2e8
Ahmed Mohammed Suleiman <ahmed2217p@gmail.com>
2026-05-30 06:24:21 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,46 @@
+const CACHE_NAME = 'lahza-cam-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/app.js'
+];
+
 self.addEventListener('install', event => {
   self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    Promise.all([
+      self.clients.claim(),
+      caches.keys().then(keys => {
+        return Promise.all(
+          keys.filter(key => key !== CACHE_NAME)
+              .map(key => caches.delete(key))
+        );
+      })
+    ])
+  );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
 });
